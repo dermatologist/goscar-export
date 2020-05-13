5e0322070dc0fd3b39fab681c74a9ca74dc8d513
beapen <github@gulfdoctor.net>
2020-05-13 15:26:21 -0400

    before FindDuplicates refactor

--- cmd/report.go
+++ cmd/report.go
@@ -14,24 +14,16 @@ import (
 //This is how you declare a global variable
 var csvMap, csvMapValid []map[string]string
 var recordCount int
-var sshHost, sshUser, sshPass, dbUser, dbPass, dbHost, dbName, dateFrom, dateTo, filePtr *string
-var sshPort, fid *int
+var dateFrom, dateTo, filePtr *string
+var fid *int
 var includeAll *bool
 
 func main() {
 	// Commandline flags
-	sshHost = flag.String("sshhost", "", "The SSH host")
-	sshPort = flag.Int("sshport", 22, "The port number")
-	sshUser = flag.String("sshuser", "ssh-user", "ssh user")
-	sshPass = flag.String("sshpass", "ssh-pass", "SSH Password")
-	dbUser = flag.String("dbuser", "dbuser", "The db user")
-	dbPass = flag.String("dbpass", "dbpass", "The db password")
-	dbHost = flag.String("dbhost", "localhost:3306", "The db host")
-	dbName = flag.String("dbname", "oscar", "The database name")
 	dateFrom = flag.String("datefrom", "oscar", "The start date")
 	dateTo = flag.String("dateto", "oscar", "The end date")
 	fid = flag.Int("fid", 1, "The eform ID")
-	filePtr = flag.String("file", "", "The csv file to process")
+	filePtr = flag.String("file", "data.csv", "The csv file to process")
 	includeAll = flag.Bool("include", false, "Include all records")
 	flag.Parse()
 
@@ -47,7 +39,8 @@ oscar_helper -sshhost=xxx -sshport=22 -sshuser=xxx -sshpass=xxx -dbuser=xxx -dbp
 	if *filePtr != "" {
 		r, err := os.Open(*filePtr)
 		if err != nil {
-			log.Panicln(err)
+			fmt.Print(usage)
+			os.Exit(1)
 		}
 		csvMap = goscar.CSVToMap(r)
 
